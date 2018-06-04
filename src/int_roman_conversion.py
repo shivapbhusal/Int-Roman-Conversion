bd40580fe4ab87b7e4a956dc13aa47ebf705cd00
shivapbhusal <civabhusal@gmail.com>
2018-06-03 21:43:59 -0400

    add draft

---
+++ src/int_roman_conversion.py
@@ -0,0 +1,15 @@
+'''
+A Python class to convert Integer to Roman and Vice Versa
+Created by Shiva Bhusal
+'''
+
+class IntRomanConverter:
+    def intToRoman(self,num):
+        return 'X'
+
+    def romanToInt(self,romanNum):
+        return '10'
+
+obj=IntRomanConverter()
+
+print(obj.romanToInt('X'))
