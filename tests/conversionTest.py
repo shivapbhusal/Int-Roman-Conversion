85b18caff3f187c62ecc83eef63508a26892f171
shivapbhusal <civabhusal@gmail.com>
2018-06-03 21:57:26 -0400

    add conVersionTest

---
+++ tests/conversionTest.py
@@ -0,0 +1,22 @@
+'''
+A test class for IntRomanConversion class.
+'''
+
+import sys
+sys.path.append(r'src') # change this if you are using Windows.  
+#print(sys.path)
+import unittest
+import int_roman_conversion as irc
+
+class TestIntRomanConverter(unittest.TestCase):
+
+    def testIntToRoman(self):
+    	converter=irc.IntRomanConverter()
+    	self.assertEqual(2,2)
+
+    def testRomanToInt(self):
+    	converter=irc.IntRomanConverter()
+    	self.assertEqual(2,2)
+
+if __name__ == '__main__':
+    unittest.main()
