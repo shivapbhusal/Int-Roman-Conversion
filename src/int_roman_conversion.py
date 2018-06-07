fe461e8db0ebedcb069d894748eb2998b8f15641
shivapbhusal <civabhusal@gmail.com>
2018-06-07 09:17:19 -0400

    add basic converter

--- src/int_roman_conversion.py
+++ src/int_roman_conversion.py
@@ -2,6 +2,7 @@
 A Python class to convert Integer to Roman and Vice Versa
 Created by Shiva Bhusal
 '''
+import sys
 
 digits={
 1:'I',
@@ -17,13 +18,35 @@ digits={
 }
 
 class IntRomanConverter:
+    def findnN(self,x):
+        n=0
+        N=0
+        while(N<x):
+            N=pow(10,n)
+            n=n+1
+        N=N/10
+        q=int(x/N)
+        return N,q
+
     def intToRoman(self,num):
         if num in digits:
-            return digits[num]          
+            return digits[num]
+        elif num==0:
+            return ''
+        else:
+            N,q =self.findnN(num)
+            print(N)
+            print(q)
+            romanValue=''
+            while(q):
+                romanValue = romanValue + digits[N]
+                q = q-1
+            rem = num % N
+            return romanValue + self.intToRoman(rem)
 
     def romanToInt(self,romanNum):
         return '10'
 
 obj=IntRomanConverter()
 
-print(obj.intToRoman(20))
+print(obj.intToRoman(int(sys.argv[1])))
